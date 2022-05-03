e02f3df500fd68f4df23ed7c735c5c3184e7aa3f
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-05-03 18:43:50 +0300

    Set up the OrderBook class

---
+++ merklerex/OrderBook.cpp
@@ -0,0 +1,9 @@
+//
+//  OrderBook.cpp
+//  merklerex
+//
+//  Created by Ahmed Shaban on 3.05.2022.
+//
+
+#include "OrderBook.hpp"
+
---
+++ merklerex/OrderBook.hpp
@@ -0,0 +1,33 @@
+//
+//  OrderBook.hpp
+//  merklerex
+//
+//  Created by Ahmed Shaban on 3.05.2022.
+//
+
+#ifndef OrderBook_hpp
+#define OrderBook_hpp
+
+#include <stdio.h>
+#include "OrderBookEntry.hpp"
+#include "CSVReader.hpp"
+
+class OrderBook
+{
+public:
+    /** construct, reading a csv data file */
+    OrderBook(std::string filename);
+    /** return vector of all know products in the dataset*/
+    std::vector<std::string> getKnownProducts();
+    /** return vector of Orders according to the sent filters*/
+    std::vector<OrderBookEntry> getOrders(OrderBookType type,
+                                          std::string product,
+                                          std::string timestamp);
+    /** return the price of the highest bid in the sent set */
+    static double getHighPrice(std::vector<OrderBookEntry>& orders);
+    /** return the price of the lowest bid in the sent set */
+    static double getLowPrice(std::vector<OrderBookEntry>& orders);
+private:
+    std::vector<OrderBookEntry> orders;
+};
+#endif /* OrderBook_hpp */
