1963229ba95ee5e827831c13094d9f37eb97de0a
ahmedMshaban <ahmed.ms.elias@gmail.com>
2022-05-03 19:19:01 +0300

    Basic OrderBook class implementation

--- merklerex/OrderBook.cpp
+++ merklerex/OrderBook.cpp
@@ -6,4 +6,23 @@
 //
 
 #include "OrderBook.hpp"
+#include "CSVReader.hpp"
+
+/** construct, reading a csv data file */
+OrderBook::OrderBook(std::string filename) {
+    orders = CSVReader::readCSV(filename);
+}
+/** return vector of all know products in the dataset*/
+std::vector<std::string> OrderBook::getKnownProducts() {
+    std::vector<std::string> products;
+    return products;
+}
+/** return vector of Orders according to the sent filters*/
+std::vector<OrderBookEntry> OrderBook::getOrders(OrderBookType type,
+                                                 std::string product,
+                                                 std::string timestamp) {
+    std::vector<OrderBookEntry> order_sup;
+    return order_sup;
+}
+
 
